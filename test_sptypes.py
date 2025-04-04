bf57c5da7809080ec8c7db03aa57d01ee4938f08
Dmitry Klykov <dyuk108@gmail.com>
2025-04-04 15:06:46 +0300

    написал star_colors.py и test_sptypes.py

---
+++ test_sptypes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Тестирование функции определения Teff по сп. классу.
+
+from star_colors import sptype2t
+
+sptypes = ('O', 'B', 'A', 'F', 'G', 'K', 'M')
+
+for spt in sptypes:
+    for sub in range(10):
+        spt_s = spt + str(sub) # строка сп. класса
+        print(spt_s, sptype2t(spt_s))
